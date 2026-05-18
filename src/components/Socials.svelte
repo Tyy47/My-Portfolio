a04b27a452668954ad3788aa9fb47d2146cff7c8
Tyy47 <tylerwmack321@gmail.com>
2026-05-18 12:15:52 -0400

    added links to social images and centered the contents

--- src/components/Socials.svelte
+++ src/components/Socials.svelte
@@ -1,22 +1,27 @@
 <script></script>
 
 <section id="socials-section">
-
-	<div class="grid-slot">
-		<div class="img-container">
-			<img src="https://assets.chanhdai.com/images/link-icons/github.webp?t=1759581475" alt="Github Logo" class="social-img" />
+	<div class="grid-container">
+		<div class="grid-slot">
+			<div class="img-container">
+				<a href="https://www.github.com/Tyy47" target="_blank"><img src="https://assets.chanhdai.com/images/link-icons/github.webp?t=1759581475" alt="Github Logo" class="social-img" /> </a>
+			</div>
+			<div class="text-container">
+				<h4><a href="https://www.github.com/Tyy47" target="_blank">Github</a></h4>
+				<p class="social-username">@Tyy47</p>
+			</div>
 		</div>
-		<div class="text-container">
-			<h4><a href="https://www.github.com/Tyy47">Github</a></h4>
-			<p class="social-username">@Tyy47</p>
+		<div class="grid-slot">
+			<div class="img-container">
+				<a href="https://www.linkedin.com/in/tyler-mack-382374310" target="_blank"><img src="https://assets.chanhdai.com/images/link-icons/linkedin.webp?t=1759581475" alt="LinkedIn Logo" class="social-img"> </a>
+			</div>
+			<div class="text-container">
+				<h4><a href="www.linkedin.com/in/tyler-mack-382374310" target="_blank">LinkedIn</a></h4>
+				<p class="social-username">Tyler Mack</p>
+			</div>
+
 		</div>
 	</div>
-	<div>Block</div>
-	<div>Block</div>
-	<div>Block</div>
-	<div>Block</div>
-
-
 </section>
 
 <style>
@@ -27,19 +32,25 @@
 	}
 
 	#socials-section {
+		border: 1px solid var(--border-color);
+		margin: 36px auto;
+		padding: 20px;
+	}
+
+	.grid-container {
 		display: grid;
 		border: 1px solid var(--border-color);
 		grid-template-columns: repeat(2, 1fr);
 		margin: 0 auto;
 		padding: 20px;
 		color: ghostwhite;
-
 	}
 
 	.grid-slot {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: nowrap;
+		justify-content: center;
 	}
 	
 	.img-container {
@@ -59,6 +70,7 @@
 		display: inline;
 		width: 50px;
 		height: 50px;
+		border-radius: 7%;
 	}
 
 </style>
