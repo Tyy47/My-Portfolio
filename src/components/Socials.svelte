c5bab50b07157051dbc2f674ab4e007b04863742
Tyy47 <tylerwmack321@gmail.com>
2026-05-18 09:19:57 -0400

    started work on socials box

---
+++ src/components/Socials.svelte
@@ -0,0 +1,64 @@
+<script></script>
+
+<section id="socials-section">
+
+	<div class="grid-slot">
+		<div class="img-container">
+			<img src="https://assets.chanhdai.com/images/link-icons/github.webp?t=1759581475" alt="Github Logo" class="social-img" />
+		</div>
+		<div class="text-container">
+			<h4><a href="https://www.github.com/Tyy47">Github</a></h4>
+			<p class="social-username">@Tyy47</p>
+		</div>
+	</div>
+	<div>Block</div>
+	<div>Block</div>
+	<div>Block</div>
+	<div>Block</div>
+
+
+</section>
+
+<style>
+
+	a {
+		color: ghostwhite;
+
+	}
+
+	#socials-section {
+		display: grid;
+		border: 1px solid var(--border-color);
+		grid-template-columns: repeat(2, 1fr);
+		margin: 0 auto;
+		padding: 20px;
+		color: ghostwhite;
+
+	}
+
+	.grid-slot {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+	}
+	
+	.img-container {
+		display: inline;
+	}
+
+	.text-container {
+		display: inline;
+		margin-left: 16px;
+	}
+
+	.social-username {
+		color: gray;
+	}
+
+	.social-img {
+		display: inline;
+		width: 50px;
+		height: 50px;
+	}
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,14 @@
 	import Header from "../components/Header.svelte";
 	import NameAndTitle from "../components/NameAndTitle.svelte";
 	import ContactMe from "../components/ContactMe.svelte";
+	import Socials from "../components/Socials.svelte";
 </script>
 
 
 <Header />
 <NameAndTitle />
 <ContactMe />
+<Socials />
 
 
 
