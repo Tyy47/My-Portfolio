840cfeb18b0cd2c30d1f3f1199aca8883340212b
Tyy47 <tylerwmack321@gmail.com>
2026-05-20 07:55:57 -0400

    continuing work on tech stack section, added basic css to get groundwork laid out.

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -2,7 +2,9 @@
 
 
 <section id="tech-stack-section">
-
+	<div id="title-statement">
+		<h4>Current Tech Stack:</h4>
+	</div>
 
 </section>
 
@@ -10,4 +12,20 @@
 
 
 
-<style></style>
+<style>
+
+	#tech-stack-section {
+		border: 1px solid var(--border-color);
+		padding: 16px;
+		color: ghostwhite;
+		margin: 0 auto;
+	}
+
+	#title-statement {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		justify-content: center;
+	}
+
+</style>
