2d24656ddb74f54046a47712fe9ec324e681e8db
Tyy47 <Tylerwmack321@gmail.com>
2026-05-21 09:20:12 -0400

    added images and links to tech stack section

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -6,6 +6,13 @@
 		<h4>Current Tech Stack:</h4>
 	</div>
 
+	<div class="imgs-container">
+		<a href="https://bun.com/" target="_blank"><img src="./Bun.png" alt="Bun" class="stack-img"></a>
+		<a href="https://typescriptlang.org/" target="_blank"><img src="./typescript.png" alt="Typescript" class="stack-img"></a>
+		<a href="https://svelte.dev" target="_blank"><img src="./svelte.png" alt="Svelte" class="stack-img"></a>
+		<a href="https://postgresql.org" target="_blank"><img src="./postgresql.png" alt="PostgreSQL" class="stack-img"></a>
+	</div>
+
 </section>
 
 
@@ -13,6 +20,10 @@
 
 
 <style>
+
+	a {
+		color: ghostwhite;
+	}
 
 	#tech-stack-section {
 		border: 1px solid var(--border-color);
@@ -26,6 +37,25 @@
 		flex-direction: row;
 		flex-wrap: nowrap;
 		justify-content: center;
+	}
+
+	.imgs-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: nowrap;
+		border: 1px solid var(--border-color);
+		padding: 20px;
+		margin-top: 16px;
+	}
+
+	.stack-img {
+		width: 75px;
+		height: 75px;
+		margin: 8px;
+	}
+
+	.stack-img:hover {
+		scale: 1.2;
 	}
 
 </style>
